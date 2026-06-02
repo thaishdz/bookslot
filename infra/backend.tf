2f90589abcb3b818df5e9d764b232f87eea032d1
Thais <thaishdzlpz@gmail.com>
2026-06-02 11:22:31 +0100

    chore: configure terraform remote backend and gitignore

---
+++ infra/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "bookslot-dev-tfstate-th33"
+    key            = "bookslot/dev/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "bookslot-dev-tflock"
+    encrypt        = true
+  }
+}
