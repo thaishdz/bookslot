e9c08cf2f35bcb6a380d3bb396c3b2ab9cd05628
Thais <thaishdzlpz@gmail.com>
2026-06-04 14:58:13 +0100

    feat: add main dynamodb table with single-table design and PITR

---
+++ infra/dynamodb.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "bookings" {
+  name         = "bookslot-dev-bookings"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "PK"
+  range_key = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
