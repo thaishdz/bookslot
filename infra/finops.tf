d0912d45b4ad62aed5ecff76f6e226b424fa7eb9
Thais <thaishdzlpz@gmail.com>
2026-07-02 16:07:06 +0100

    feat: add finops

---
+++ infra/finops.tf
@@ -0,0 +1,34 @@
+resource "aws_budgets_budget" "bookslot_monthly" {
+  name         = "bookslot-dev-monthly"
+  budget_type  = "COST"
+  limit_amount = "10.0" # limite de mi cuenta de estudiante
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  # Aviso temprano: 50% del presupuesto (5$)
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.alarm_email]
+  }
+
+  # Alerta: 80% del presupuesto (8$)
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.alarm_email]
+  }
+
+  # Predictivo: se prevé superar el 100% (10$)
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.alarm_email]
+  }
+}
